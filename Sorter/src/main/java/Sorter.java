469b8f46fce37804e374d43084a536c455ce39e4
rafaguerrero <rguerreroclemente@gmail.com>
2016-11-01 18:11:22 +0100

    Sorter finished

--- Sorter/src/main/java/Sorter.java
+++ Sorter/src/main/java/Sorter.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Sorter {
 
@@ -23,6 +20,37 @@ public class Sorter {
 
 
     public static void main(String[] args) {
-        System.out.println("hola que ase");
+        List<String> unassigned = new ArrayList<>(PARTICIPANTS);
+        Map<String, String> solution = new HashMap<>();
+
+        sort(unassigned, solution);
+
+        solution.forEach((k,v)->System.out.println(k + " gives present to : " + v));
+    }
+
+    private static boolean sort(List<String> unassigned, Map<String, String> solution) {
+        if(unassigned.isEmpty()) {
+            return true;
+        }
+
+        int random = (int) Math.floor(Math.random() * unassigned.size());
+
+        String current = unassigned.get(random);
+        unassigned.remove(current);
+
+        List<String> candidates = Arrays.asList(RULES.get(current).split(","));
+
+        for(String candidate : candidates) {
+            solution.put(current, candidate);
+
+            if(sort(unassigned, solution)) {
+                return true;
+            }
+        }
+
+        unassigned.add(current);
+        solution.remove(current);
+
+        return false;
     }
 }
